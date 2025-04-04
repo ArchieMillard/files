58a6bd64c49002032319097b4a6de8aa80954aec
Archie Millard <84399154+ArchieMillard@users.noreply.github.com>
2025-04-04 16:42:44 +0100

    Update debianConfig.sh

--- deployment/debian/debianConfig.sh
+++ deployment/debian/debianConfig.sh
@@ -6,7 +6,7 @@ sudo apt install gnome
 # Remove unwanted packages.
 sudo apt purge shotwell gnome-games evolution rhythmbox gnome-software gnome-sound-recorder
 # Install wanted packages.
-sudo apt install bash binutils bison coreutils diffutils findutils gawk gcc grep gzip audacity gimp vlc htop vim gnome-boxes gparted pavucontrol neofetch cmus xmonad xmobar libghc-xmonad-extras-dev ghc yt-dlp links brasero sound-juicer picard git xfonts-terminus fonts-terminus nala qjackctl geary make virt-manager gnome-firmware fwupdate vcdimager arch-install-scripts synaptic software-properties-gtk lshw unattended-upgrades vrms distrobox podman keepassxc dconf-editor gnome-console devede handbrake inkscape gnome-tweaks obs-studio v4l2loopback-dkms libxcb-xinerama0 libxcb-cursor0 libnss3
+sudo apt install bash binutils bison coreutils diffutils findutils gawk gcc grep gzip audacity gimp vlc htop vim gnome-boxes gparted pavucontrol neofetch cmus xmonad xmobar libghc-xmonad-extras-dev ghc yt-dlp links brasero sound-juicer cd-paranoia picard git xfonts-terminus fonts-terminus nala qjackctl geary make virt-manager gnome-firmware fwupdate vcdimager arch-install-scripts synaptic software-properties-gtk lshw unattended-upgrades vrms distrobox podman keepassxc dconf-editor gnome-console devede handbrake inkscape gnome-tweaks obs-studio v4l2loopback-dkms libxcb-xinerama0 libxcb-cursor0 libnss3
 # Remove other wanted packages after installation of wanted packages.
 sudo apt purge imagemagick gnome-terminal
 # Configure auto-upgrades.
