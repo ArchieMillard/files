48d3e2b555064de3170a6881d4ec560bd48b8189
Archie Millard <84399154+ArchieMillard@users.noreply.github.com>
2025-04-04 17:46:30 +0100

    Update cd-paranoia.sh

--- scripts/cd-paranoia.sh
+++ scripts/cd-paranoia.sh
@@ -1,1 +1,3 @@
-cd-paranoia -Bzw
+#! /bin/sh
+# Extracts a CD as individual tracks, in WAV, and will infinitley repair reading errors (you may have to disable this option if it loops forever)
+cd-paranoia -Bwz
